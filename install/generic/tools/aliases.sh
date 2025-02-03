393d01b7934118962af96a86c109d3b59a9fd468
Alex Pope <67066474+aroundwithalex@users.noreply.github.com>
2025-02-03 17:58:42 +0000

    Rename file

---
+++ install/generic/tools/aliases.sh
@@ -0,0 +1,3 @@
+echo "alias sommelier-update='~/.local/share/sommelier/update.sh'" >> ~/.config/fish/config.fish
+echo "alias sommelier-install='~/.local/share/sommelier/install.sh'" >> ~/.config/fish/config.fish
+echo "alias sommelier-uninstall='~/.local/share/sommelier/uninstall.sh'" >> ~/.config/fish/config.fish
--- install/generic/tools/sommelier.sh
+++
@@ -1,3 +0,0 @@
-echo "alias sommelier update='~/.local/share/sommelier/update.sh'"
-echo "alias sommelier install='~/.local/share/sommelier/install.sh'"
-echo "alias sommelier uninstall='~/.local/share/sommelier/uninstall.sh'"
