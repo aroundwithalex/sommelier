f9b4ec834e0fba60de1084f258374819faa02bdb
Alex Pope <67066474+aroundwithalex@users.noreply.github.com>
2025-02-04 18:31:42 +0000

    Fix bug in downloading desktop images

--- style/backgrounds.sh
+++ style/backgrounds.sh
@@ -1,4 +1,6 @@
 printf "$(tput setaf 2)\n Installing desktop backgrounds\n"
+
+rm -rf /tmp/desktop-backgrounds
 git clone https://github.com/aroundwithalex/desktop-backgrounds.git --single-branch /tmp/desktop-backgrounds > /dev/null
 
 cd /tmp/desktop-backgrounds
