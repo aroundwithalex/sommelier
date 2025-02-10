59bf8acf0ac07a771a7ed03be545073c548d959c
Alex Pope <67066474+aroundwithalex@users.noreply.github.com>
2025-02-10 22:53:10 +0000

    Use run.sh rather than install.sh for sommelier-install

--- install/generic/tools/aliases.sh
+++ install/generic/tools/aliases.sh
@@ -1,6 +1,6 @@
 printf "$(tput setaf 2)\n Adding sommelier aliases\n"
 echo "alias sommelier-update='~/.local/share/sommelier/update.sh'" >> ~/.config/fish/config.fish
-echo "alias sommelier-install='~/.local/share/sommelier/install.sh'" >> ~/.config/fish/config.fish
+echo "alias sommelier-install='~/.local/share/sommelier/run.sh'" >> ~/.config/fish/config.fish
 echo "alias sommelier-uninstall='~/.local/share/sommelier/uninstall.sh'" >> ~/.config/fish/config.fish
 echo "alias sommelier-help='~/.local/share/sommelier/help.sh'" >> ~/.config/fish/config.fish
 printf "$(tput setaf 2)\n Aliases added\n"
