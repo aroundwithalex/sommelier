782db58607e9bceaf3e9c0148580e4d7f43e60d8
Alex Pope <67066474+aroundwithalex@users.noreply.github.com>
2025-02-15 23:34:44 +0000

    Fix configuration loading

--- install/fedora/ghostty.sh
+++ install/fedora/ghostty.sh
@@ -1,8 +1,8 @@
 printf "$(tput setaf 2)\n Installing ghostty\n"
 sudo dnf copr enable pgdev/ghostty
 sudo dnf install -y ghostty
+cp ~/.local/share/sommelier/cfgs/ghostty/config ~/.config/ghostty/
 printf "$(tput setaf 2)\n ghostty successfully installed\n"
 
-cd ../../cfgs/ghostty
 
 cp config ~/.config/ghostty/
