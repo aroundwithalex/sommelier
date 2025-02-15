078c9faf064d7f66d168e65afdba49a4d73c6976
Alex Pope <67066474+aroundwithalex@users.noreply.github.com>
2025-02-15 23:37:19 +0000

    Remove broken config line

--- install/fedora/ghostty.sh
+++ install/fedora/ghostty.sh
@@ -3,6 +3,3 @@ sudo dnf copr enable pgdev/ghostty
 sudo dnf install -y ghostty
 cp ~/.local/share/sommelier/cfgs/ghostty/config ~/.config/ghostty/
 printf "$(tput setaf 2)\n ghostty successfully installed\n"
-
-
-cp config ~/.config/ghostty/
